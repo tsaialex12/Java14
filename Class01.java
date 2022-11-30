6888be34d2d5b039b412cced18f3d5d8a3c71312
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2022-11-30 11:17:32 +0800

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class hw02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in)
+		String str ;
+		System.out.print("輸入字串");
+		str=scn.nextLine();
+		System.out.print("轉換成大寫:)"+str.toUpperCase());
+	}
+
+}
